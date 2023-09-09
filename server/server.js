7699a7e1fc1cc4b5724b806c49e786dc4acbab33
Akshat2k3 <akshat.sharma2k3@gmail.com>
2023-09-09 09:14:55 -0500

    Adding backend routes/endpoints for front-end development with temporary local database

--- server/server.js
+++ server/server.js
@@ -1,8 +1,45 @@
 const express = require('express')
-const app = express()
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const PORT = 5000;
+const dbPath = path.join(__dirname, 'data/db.json');
 
-app.get("/api", (req, res) => {
-    res.json({ "objects" : ["objectOne", "objectTwo", "objectThree"] })
-})
+app.use(express.json());
 
-app.listen(5000, () => { console.log("Server started on port 5000.") })
+app.get('/api/users', (req, res) => {
+    fs.readFile(dbPath, 'utf-8', (err, data) => {
+        if(err) {
+            res.status(500).send('Server Error');
+            return;
+        }
+        const { users } = JSON.parse(data);
+        res.json(users);
+    });
+});
+
+app.post('/api/users', (req, res) => {
+    fs.readFile(dbPath, 'utf-8', (err, data) => {
+        if(err) {
+            res.status(500).send('Server Error');
+            return;
+        }
+        const db = JSON.parse(data);
+        const newUser = {
+            id: db.users.length + 1,
+            ...req.body
+        }
+        db.users.push(newUser);
+        fs.writeFile(dbPath, JSON.stringify(db, null, 2), (err) => {
+            if (err) {
+                res.stastus(500).send('Server Error');
+                return;
+            }
+            res.status(201).json(newUser);
+        });
+    });
+});
+
+app.listen(PORT, () => { 
+    console.log(`Server started on port: ${PORT}.`) 
+});
